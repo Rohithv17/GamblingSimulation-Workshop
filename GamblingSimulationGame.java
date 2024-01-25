364353bf74df0a24f120263c75efac0fc826829d
Rohithv17 <rohithv1396@gmail.com>
2024-01-25 11:59:58 +0530

    Added Win and Loss calculating functionality

--- GamblingSimulationGame.java
+++ GamblingSimulationGame.java
@@ -1,14 +1,42 @@
 package com.bridgelabz.GamblingSimulation;
-
+import java.lang.Math;
 public class GamblingSimulationGame {
+
+        int Stake=100;
+        int BetPerGame=1;
+
+
+     void GameRule(){}
+
+
+        void Compute(){
+        //For every bet remaining money reduces by 1
+        int RemainingMoney=Stake-BetPerGame;
+        double randno=Math.random();
+//Calculating the Win and loss
+        if(randno<=0.5){
+            System.out.println("You lose ");
+
+            System.out.println("Remaining Money is "+RemainingMoney);
+
+        }
+        else {
+            System.out.println("You win");
+            RemainingMoney=RemainingMoney+1;
+            System.out.println("Remaining Money is "+RemainingMoney);
+        }
+    }
 
 
 
     public static void main(String[] args) {
-     int Stake=100;
-     int BetPerGame=1;
-     int RemainingMoney=Stake-BetPerGame;
+
+      GamblingSimulationGame G = new GamblingSimulationGame();
+      G.Compute();
+
+
     }
+
 
 
 

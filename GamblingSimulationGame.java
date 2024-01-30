4135a336885b0b54bdebfc698308b9b0635b8cc1
Rohithv17 <rohithv1396@gmail.com>
2024-01-30 13:03:40 +0530

    added feature to calculate no. of unlucky and lucky days

--- GamblingSimulationGame.java
+++ GamblingSimulationGame.java
@@ -8,41 +8,48 @@ public class GamblingSimulationGame {
         void Compute() {
             //For every bet remaining money reduces by 1
             int RemainingMoney = Stake - BetPerGame;
-            int hourswon=0,hourslost=0,dayswon=0,dayslost=0;
-            int moneywon=0,moneylost=0;  //stores the value of money lost and money won //
-            for(int i=1;i<=20;i++){   // To calculate the money won and lost for 20 days
-           for(int j=0;j<=24;j++){    // To calculate the number of  plays per day(24 hours,1 chance per hr)
-               double randno = Math.random(); //generating random number to calculate chances
-               if(randno<=0.5){     //To Compute for loss
-                   moneylost=moneylost+1;
-                   hourslost=hourslost+1;
-                   dayslost=hourslost/24;
+            int hourswon = 0, hourslost = 0,unluckiesthour=0,luckiesthour=0;
+            int unluckiestday = 0, luckiestday = 0;  //stores the value of luckiest day (max wins) and unluckiest day(max losses) //
+            for (int i = 1; i <= 20; i++) {   // To calculate the money won and lost for 20 days
+                for (int j = 0; j <= 24; j++) {    // To calculate the number of  plays per day(24 hours,1 chance per hr)
+                    double randno = Math.random(); //generating random number to calculate chances
 
 
-               }
-               else {  //To compute for win
-                   moneywon=moneywon+1;
-                   RemainingMoney=RemainingMoney+1;
-                   hourswon=hourswon+1;
-                   dayswon=hourswon/24;
+
+                    if (randno <= 0.5) {     //To Compute for loss
+                        unluckiesthour=unluckiesthour+1;
+                        hourslost = hourslost + 1;
 
 
-               }
-           }}
-            System.out.println("money lost :"+moneylost + " || Days lost : " + dayslost);
-            System.out.println("money won :" + moneywon +" || Days won: "+dayswon);
+                    }
+                    else {  //To compute for win
+                        luckiesthour=luckiesthour+1;
+                        hourswon = hourswon + 1;
+
+                    }
+
+                }
+                if(luckiesthour>unluckiesthour){
+                    luckiestday=luckiestday+1;
+                }
+                else{
+                    unluckiestday=unluckiestday+1;
+                }
+            }
+
+
+                System.out.println("Luckiest Days :" + luckiestday);
+                System.out.println("Unluckiest Days:" + unluckiestday);
 
             }
 
-            public static void main(String[] args) {
+            public static void main (String[]args){
 
-      GamblingSimulationGame G = new GamblingSimulationGame();
-      G.Compute();
-
-
-    }
+                GamblingSimulationGame G = new GamblingSimulationGame();
+                G.Compute();
 
 
+            }
 
 
-}
+        }
